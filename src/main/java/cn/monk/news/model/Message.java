f8a5b33b33e03b8df160db6b1c4afc26a3624c08
15csmonk <15jyli5@stu.edu.cn>
2019-01-08 22:34:56 +0800

    Initial commit

---
+++ src/main/java/cn/monk/news/model/Message.java
@@ -0,0 +1,72 @@
+package cn.monk.news.model;
+
+import java.util.Date;
+
+/**
+ * Created by 和尚 on 2019/1/8.
+ */
+public class Message {
+    private int id;
+    private int fromId;
+    private int toId;
+    private String content;
+    private Date createDate;
+    private int hasRead;
+    private String conversationId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getFromId() {
+        return fromId;
+    }
+
+    public void setFromId(int fromId) {
+        this.fromId = fromId;
+    }
+
+    public int getToId() {
+        return toId;
+    }
+
+    public void setToId(int toId) {
+        this.toId = toId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public int getHasRead() {
+        return hasRead;
+    }
+
+    public void setHasRead(int hasRead) {
+        this.hasRead = hasRead;
+    }
+
+    public String getConversationId() {
+        return conversationId;
+    }
+
+    public void setConversationId(String conversationId) {
+        this.conversationId = conversationId;
+    }
+}
