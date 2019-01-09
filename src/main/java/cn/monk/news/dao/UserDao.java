3b96d11b72f9da18e675ec44f621e9e01e469ff8
15csmonk <15jyli5@stu.edu.cn>
2019-01-09 21:06:03 +0800

    Initial commit

--- src/main/java/cn/monk/news/dao/UserDao.java
+++ src/main/java/cn/monk/news/dao/UserDao.java
@@ -1,9 +1,11 @@
 package cn.monk.news.dao;
 
+import org.apache.ibatis.annotations.Mapper;
+
 /**
  * Created by 和尚 on 2019/1/9.
  */
-
+@Mapper
 public class UserDao {
 
 }
--- src/main/java/cn/monk/news/service/UserService.java
+++ src/main/java/cn/monk/news/service/UserService.java
@@ -1,5 +1,6 @@
 package cn.monk.news.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
