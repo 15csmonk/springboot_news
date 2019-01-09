b0afd6ca6cd0eb5ba66b3ca35b5a2bc74f0bd816
15csmonk <15jyli5@stu.edu.cn>
2019-01-09 21:46:29 +0800

    Initial commit

--- src/main/java/cn/monk/news/dao/UserDao.java
+++ src/main/java/cn/monk/news/dao/UserDao.java
@@ -7,5 +7,8 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface UserDao {
+    String TABLE_NAME = "user";
+    String INSET_FIELDS = " name, password, salt, head_url ";
+    String SELECT_FIELDS = " id, name, password, salt, head_url";
 
 }
