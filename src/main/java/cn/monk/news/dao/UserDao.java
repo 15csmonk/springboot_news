6147d6d82b5fa181b99148cfc0d658307847bd22
15csmonk <15jyli5@stu.edu.cn>
2019-01-10 22:56:47 +0800

    Initial commit

--- src/main/java/cn/monk/news/dao/UserDao.java
+++ src/main/java/cn/monk/news/dao/UserDao.java
@@ -1,8 +1,7 @@
 package cn.monk.news.dao;
 
 import cn.monk.news.model.User;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.*;
 
 /**
  * Created by 和尚 on 2019/1/9.
@@ -13,7 +12,18 @@ public interface UserDao {
     String INSET_FIELDS = " name, password, salt, head_url ";
     String SELECT_FIELDS = " id, name, password, salt, head_url";
 
-    @Insert({"insert into ", TABLE_NAME, "(", INSET_FIELDS,
-            ") values (#{name},#{password},#{salt},#{headUrl})"})
+    @Insert({"insert into ", TABLE_NAME, "(", INSET_FIELDS, ") values (#{name},#{password},#{salt},#{headUrl})"})
     int addUser(User user);
+
+    @Select({"select", SELECT_FIELDS, "from", TABLE_NAME, "where id=#{id}"})
+    User selectById(int id);
+
+    @Select({"select", SELECT_FIELDS, "from", TABLE_NAME, "where name=#{name}"})
+    User selectByName(String name);
+
+    @Update({"update ", TABLE_NAME, " set password=#{password} where id=#{id}"})
+    void updatePassword(User user);
+
+    @Delete({"delete from ", TABLE_NAME, " where id=#{id}"})
+    void deleteById(int id);
 }
