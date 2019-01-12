9ce4a2083246b91f06d851961d158555a61d389a
15csmonk <15jyli5@stu.edu.cn>
2019-01-12 15:22:46 +0800

    Initial commit

--- src/main/java/cn/monk/news/controller/HomeController.java
+++ src/main/java/cn/monk/news/controller/HomeController.java
@@ -1,10 +1,67 @@
 package cn.monk.news.controller;
 
+import cn.monk.news.model.EntityType;
+import cn.monk.news.model.HostHolder;
+import cn.monk.news.model.News;
+import cn.monk.news.model.ViewObject;
+import cn.monk.news.service.LikeService;
+import cn.monk.news.service.NewsService;
+import cn.monk.news.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by 和尚 on 2019/1/12.
  */
 @Controller
 public class HomeController {
+    @Autowired
+    private NewsService newsService;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private HostHolder hostHolder;
+    @Autowired
+    private LikeService likeService;
+
+    private List<ViewObject> getNews(int userId, int offset, int limit) {
+        List<News> newsList = newsService.getLatestNews(userId, offset, limit);
+        int localUserId = hostHolder.getUser() != null ? hostHolder.getUser().getId() : 0;
+        List<ViewObject> vos = new ArrayList<>();
+        for (News news : newsList) {
+            ViewObject vo = new ViewObject();
+            vo.set("news", news);
+            vo.set("user", userService.getUser(news.getUserId()));
+//            if (localUserId != 0) {
+//                vo.set("like", likeService.getLikeStatus(localUserId, EntityType.ENTITY_NEWS, news.getId()));
+//            } else {
+//                vo.set("like", 0);
+//            }
+            vos.add(vo);
+        }
+        return vos;
+    }
+
+    @RequestMapping(path = {"/", "/index"}, method = {RequestMethod.GET, RequestMethod.POST})
+    public String index(@RequestParam(value = "pop", defaultValue = "0") int pop, Model model) {
+        model.addAttribute("vos",getNews(0,0,10));
+        if (hostHolder.getUser() != null) {
+            pop = 0;
+        }
+        model.addAttribute("pop", pop);
+        return "home";
+    }
+    @RequestMapping(path = {"/user/{userId}"}, method = {RequestMethod.GET, RequestMethod.POST})
+    public String userIndex(Model model, @PathVariable("userId") int userId) {
+        model.addAttribute("vos", getNews(userId, 0, 10));
+        return "home";
+    }
 }
---
+++ src/main/java/cn/monk/news/service/LikeService.java
@@ -0,0 +1,11 @@
+package cn.monk.news.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by 和尚 on 2019/1/12.
+ */
+@Service
+public class LikeService {
+
+}
