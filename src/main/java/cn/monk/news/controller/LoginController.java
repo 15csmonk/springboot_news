863c7f6284072896e23bdebb517b6feefbe56c67
15csmonk <15jyli5@stu.edu.cn>
2019-01-19 00:41:22 +0800

    Initial commit

--- src/main/java/cn/monk/news/controller/LoginController.java
+++ src/main/java/cn/monk/news/controller/LoginController.java
@@ -1,5 +1,6 @@
 package cn.monk.news.controller;
 
+import cn.monk.news.model.News;
 import cn.monk.news.service.UserService;
 import cn.monk.news.util.NewsUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,29 +17,31 @@ import java.util.Map;
 @Controller
 public class LoginController {
     @Autowired
-    private UserService userService;
+    UserService userService;
     @RequestMapping(path = {"/reg/"}, method = {RequestMethod.GET, RequestMethod.POST})
     @ResponseBody
     public String reg(@RequestParam("username") String username,
                       @RequestParam("password") String password,
-                      @RequestParam(value = "remember", defaultValue = "0") int rememberme,
-                      HttpServletResponse response){
-        try{
+                      @RequestParam(value="rember", defaultValue = "0") int rememberme,
+                      HttpServletResponse response) {
+        try {
             Map<String, Object> map = userService.register(username, password);
-            if(map.containsKey("ticket")){
+            if (map.containsKey("ticket")) {
                 Cookie cookie = new Cookie("ticket", map.get("ticket").toString());
                 cookie.setPath("/");
-                if(rememberme > 0){
+                if (rememberme > 0) {
                     cookie.setMaxAge(3600*24*5);
                 }
                 response.addCookie(cookie);
                 return NewsUtil.getJSONString(0, "注册成功");
-            }else{
+            } else {
                 return NewsUtil.getJSONString(1, map);
             }
-        }catch (Exception e){
+
+        } catch (Exception e) {
+            //  logger.error("注册异常" + e.getMessage());
             System.out.println(e.getMessage());
-            return NewsUtil.getJSONString(1, "注册一场");
+            return NewsUtil.getJSONString(1, "注册异常");
         }
     }
 
@@ -47,22 +50,23 @@ public class LoginController {
     public String login(@RequestParam("username") String username,
                         @RequestParam("password") String password,
                         @RequestParam(value="rember", defaultValue = "0") int rememberme,
-                        HttpServletResponse response){
-        try{
+                        HttpServletResponse response)
+    {
+        try {
             Map<String, Object> map = userService.login(username, password);
-            if (map.containsKey("ticket")){
+            if (map.containsKey("ticket")) {
                 Cookie cookie = new Cookie("ticket", map.get("ticket").toString());
                 cookie.setPath("/");
-                if (rememberme > 0){
+                if (rememberme > 0) {
                     cookie.setMaxAge(3600*24*5);
                 }
                 response.addCookie(cookie);
                 return NewsUtil.getJSONString(0, "登陆成功");
-            } else{
+            } else {
                 return NewsUtil.getJSONString(1, map);
             }
 
-        } catch (Exception e){
+        } catch (Exception e) {
             //   logger.error("注册异常" + e.getMessage());
             System.out.println(e.getMessage());
             return NewsUtil.getJSONString(1, "登陆异常");
